f10447b140e7ae3eb007f860e3b2df6d846a3e71
跨性别 <cvyl@pm.me>
2024-06-16 16:44:42 +0200

    feat: remove unused code

--- src/app/(dash)/app/page.tsx
+++ src/app/(dash)/app/page.tsx
@@ -2,11 +2,7 @@ import React, { Suspense } from 'react'
 import Loading from './loading'
 import DosageComponent from '@/components/_app/dosagechart'
 
-export default async function Page({
-	searchParams
-}: {
-	searchParams: Record<string, string | string[] | undefined>
-}) {
+export default async function Page() {
 	return (
 		<>
 			<h1>Home</h1>
